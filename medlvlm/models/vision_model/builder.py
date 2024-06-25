8c0022ad63a5c43bc49010c3a505d7ceeb92bc3c
ngocson1042002 <20280080@student.hcmus.edu.vn>
2024-06-25 18:41:54 +0700

    add biomed_pubmed_clip

--- medlvlm/models/vision_model/builder.py
+++ medlvlm/models/vision_model/builder.py
@@ -1,6 +1,7 @@
 from .eva_vit import create_eva_vit_g
 from .clip_vit import create_pubmed_clip_vit
 from .biomed_clip import create_biomed_clip
+from .biomed_pubmed_clip import create_biomed_pubmed_clip
 
 def build_vision_encoder(vision_model, **kwargs):
     if vision_model == "eva_clip_g":
@@ -9,10 +10,15 @@ def build_vision_encoder(vision_model, **kwargs):
     if vision_model == "pubmed_clip_vit":
         img_size = kwargs["img_size"]
         assert img_size == 224, "The resolution of the image must be (224, 224)"
-        num_concat = 7
+        num_concat = 1
         return create_pubmed_clip_vit(**kwargs), num_concat
     if vision_model == "biomed_clip":
         img_size = kwargs["img_size"]
         assert img_size == 224, "The resolution of the image must be (224, 224)"
         num_concat = 4
         return create_biomed_clip(**kwargs), num_concat
+    if vision_model == "biomed_pubmed_clip":
+        img_size = kwargs["img_size"]
+        assert img_size == 224, "The resolution of the image must be (224, 224)"
+        num_concat = 6
+        return create_biomed_pubmed_clip(**kwargs), num_concat
